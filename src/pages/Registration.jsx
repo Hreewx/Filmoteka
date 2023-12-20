6f98d7a9ccd8fb6db7a37eba8bad437e59565aeb
Hreewx <aknyshev1337@gmail.com>
2023-12-20 23:21:16 +0300

    Added starter folder structure and css reset

---
+++ src/pages/Registration.jsx
@@ -0,0 +1,5 @@
+function Registration() {
+  return <div>Registration page</div>;
+}
+
+export default Registration;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import Logo from "./Logo";
+import NavBar from "./NavBar";
+
+function Header() {
+  return (
+    <header>
+      <Logo />
+      Header
+      <NavBar />
+    </header>
+  );
+}
+
+export default Header;
